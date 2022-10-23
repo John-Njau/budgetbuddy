1382e927274b2eff4936b0be410322eaf194f581
John Njau <johnnjaunjoroge@gmail.com>
2022-10-23 12:39:08 +0300

    Added expenses api endpoints

--- budget_backend/expenses/serializers.py
+++ budget_backend/expenses/serializers.py
@@ -9,6 +9,16 @@ class ExpenseSerializer(serializers.ModelSerializer):
         model = Expense
         fields = '__all__'
 
+    def create(self, validated_data):
+        return Expense.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.category = validated_data.get('category', instance.category)
+        instance.save()
+        return instance
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
