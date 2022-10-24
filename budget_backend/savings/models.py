dc1c57057cad96c69ce885667d52fcdfc6437bc0
John Njau <johnnjaunjoroge@gmail.com>
2022-10-24 06:29:26 +0300

    Added savings app

--- budget_backend/savings/models.py
+++ budget_backend/savings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class SavingTarget(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.FloatField()
+    notes = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Saving Target"
+        verbose_name_plural = "Saving Targets"
+
+
+class Saving(models.Model):
+    amount = models.FloatField()
+    notes = models.CharField(max_length=255)
+    target_amount = models.ForeignKey(SavingTarget, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.amount
--- budget_backend/savings/views.py
+++ budget_backend/savings/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from savings.models import Saving
+from savings.serializers import SavingSerializer
+
 
 # Create your views here.
+class SavingsView(APIView):
+    def get(self, request):
+        saving = Saving.objects.all()
+        serializer = SavingSerializer(saving, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SavingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        saving = Saving.objects.get(pk=pk)
+        serializer = SavingSerializer(saving, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        saving = Saving.objects.get(pk=pk)
+        saving.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
